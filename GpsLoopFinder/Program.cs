0b8e47b480458f8063226c7bd5fc2c532a3c84f6
Goblinus <storedenen@gmail.com>
2023-02-02 13:18:33 +0100

    Fixed bearing calculation

--- GpsLoopFinder/Program.cs
+++ GpsLoopFinder/Program.cs
@@ -18,7 +18,7 @@ foreach (var feature in layer)
             foreach (var point in points)
             {
                 var distance = CalculateDistance(lastPoint.X, lastPoint.Y, point.X, point.Y);
-                var speed = CalcualteSpeed(distance, 1d) * 3.6d;
+                var speed = CalculateSpeed(distance, 1d) * 3.6d;
                 var bearing = CalculateBearing(lastPoint.X, lastPoint.Y, point.X, point.Y);
                 Console.WriteLine($"Longitude: {point.X}, Latitude: {point.Y}, Distance:{distance} M, Speed: {speed} KM/H, Bearing: {bearing}");
                 lastPoint = point;
@@ -49,16 +49,41 @@ double CalculateDistance(double startLon, double startLat, double endLon, double
     return result;
 }
 
+/*
+47.88189778116764, 20.37680305248897
+47.88189180427088, 20.37681142192834
+Distance:0,9117285828999017 M, 
+Speed: 3,282222898439646 KM/H, 
+Bearing: 136,79807491415391
+*/
+
 double CalculateBearing(double startLon, double startLat, double endLon, double endLat)
 {
-    var y = Math.Sin(endLon - startLon) * Math.Cos(endLat);
-    var x = Math.Cos(startLat) * Math.Sin(endLat) - 
-            Math.Sin(startLat) * Math.Cos(endLat) * Math.Cos(endLon - startLon);
-    var angle = Math.Atan2(y, x);
-    return (angle * 180 / Math.PI) % 360;
+    double dLon = ToRadians(endLon - startLon);
+    var startLatRad = ToRadians(startLat);
+    var endLatRad = ToRadians(endLat);
+
+    double y = Math.Sin(dLon) * Math.Cos(endLatRad);
+    double x = Math.Cos(startLatRad) * Math.Sin(endLatRad) -
+               Math.Sin(startLatRad) * Math.Cos(endLatRad) * Math.Cos(dLon);
+    double bearing = Math.Atan2(y, x);
+    bearing = ToDegrees(bearing);
+    bearing = (bearing + 360) % 360;
+
+    return bearing;
 }
 
-double CalcualteSpeed(double distanceInMeter, double timeInSeconds)
+double ToRadians(double angle)
+{
+    return Math.PI * angle / 180.0;
+}
+
+double ToDegrees(double radians)
+{
+    return radians * 180 / Math.PI;
+}
+
+double CalculateSpeed(double distanceInMeter, double timeInSeconds)
 {
     return distanceInMeter / timeInSeconds;
 }
